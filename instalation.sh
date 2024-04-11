d2fb16b8dc1b01b63c42e8a5956505a796a8f6f9
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-11 18:05:23 +0000

    feat: :memo: Se agrega extensiones

--- instalation.sh
+++ instalation.sh
@@ -1,1 +1,18 @@
-#!
+#!/bin/bash
+#Define el array de extensiones
+extensiones=(
+    "mhutchie.git-graph"
+    "teabyii.ayu"
+    "vivaxy.vscode-conventional-commits"
+    "file-icons.file-icons"
+    "pranaygp.vscode-css-peek"
+    "rangav.vscode-thunder-client"
+    "ms-python.python"
+    "aykutsarac.jsoncrack-vscode"
+    "yandeu.five-server"
+)
+
+for extension in "${extensiones[@]}"
+do
+    code --install-extension "$extension"
+done
